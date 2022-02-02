e61b32da0fd02944f23893625b3d2887ae525a5b
Ali <ali-ozdemirov@mail.ru>
2022-02-02 17:37:35 +0300

    profile

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const PORT = 3000;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const profileRouter = require('./routes/profile')
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/profile', profileRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
