f7afa27453bae99c45930106c03a12d2bb270934
AlexGunin <iwaannafly@mail.ru>
2022-02-04 10:52:34 +0300

    add localstorage bucket

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -3,6 +3,10 @@ const buttonClear = document.querySelector('i.clear');
 const buttonSearch = document.querySelector('i.search');
 const gridLayout = document.querySelector('.grid-layout');
 const spanCount = document.querySelector('span.count');
+const searchWrap = document.querySelector('.search-wrap');
+const logo = document.querySelector('.logo');
+const ulNav = document.querySelector('ul.nav');
+
 buttonClear.addEventListener('click', () => {
   inputSearch.value = '';
 });
@@ -18,6 +22,16 @@ document.addEventListener('DOMContentLoaded', async () => {
   const result = await response.json();
   gridLayout.classList.add('appear');
   gridLayout.innerHTML = createHTML(result.allCards);
+  gridLayout.addEventListener('click', (event) => {
+    console.log(event.target);
+    if (event.target.classList.contains('buy')) {
+      addToBucket(event);
+      updateBucket();
+      createControl(event);
+    } else if (event.target.classList.contains('plus') || event.target.classList.contains('minus')) {
+      controlBucket(event);
+    }
+  });
   updateBucket();
 });
 
@@ -56,8 +70,8 @@ buttonSearch.addEventListener('click', async () => {
 
 function addToBucket(event) {
   const { id } = event.target.closest('.card-wrap').dataset;
-  console.log(id);
   const bucket = JSON.parse(localStorage.getItem('bucket'));
+
   if (bucket) {
     const index = bucket.findIndex((card) => +card.id === +id);
     if (index > -1) {
@@ -79,23 +93,16 @@ function addToBucket(event) {
   }
 }
 
-gridLayout.addEventListener('click', (event) => {
-  if (event.target.classList.contains('buy')) {
-    addToBucket(event);
-    updateBucket();
-    createControl(event);
-  }
-});
-
 function createControl(event) {
   const { target } = event;
   const buyWrap = event.target.closest('.buy-wrap');
-  target.classList.add('disappear');
+  const { id } = event.target.closest('.card-wrap').dataset;
+  const bucket = localStorage.getItem('bucket');
+  target.classList.add('disappear', 'events-none');
   setTimeout(() => {
     target.remove();
   }, 500);
   buyWrap.innerHTML = '<div class = "control-wrap"><button class = "btn btn-success minus">-</button><span class="card-count">1 шт</span></span><button class = "btn btn-success plus">+</button></div>';
-  buyWrap.addEventListener('click', controlBucket);
 }
 
 function updateCount(parent, newValue) {
@@ -111,14 +118,16 @@ function controlBucket(event) {
   const controlWrap = event.target.closest('.control-wrap');
   if (event.target.classList.contains('minus')) {
     bucket[indexCard].count--;
-    updateCount(buyWrap, bucket[indexCard].count);
     if (bucket[indexCard].count === 0) {
-      controlWrap.classList.add('disappear');
+      bucket.splice(indexCard, 1);
+      controlWrap.classList.add('disappear', 'events-none');
       setTimeout(() => {
         controlWrap.remove();
+        buyWrap.classList.add('appear');
         buyWrap.innerHTML = '<button class = "btn btn-success buy">В корзину</button>';
       }, 300);
     }
+    updateCount(buyWrap, bucket[indexCard].count);
   } else if (event.target.classList.contains('plus')) {
     bucket[indexCard].count++;
     updateCount(buyWrap, bucket[indexCard].count);
@@ -134,7 +143,16 @@ function createHTML(arr) {
   const bucket = JSON.parse(localStorage.getItem('bucket')) ?? [];
   const allCards = arr.map((card) => {
     const data = new Date(card.createdAt);
-    const storageIndex = bucket.findIndex((item) => card.id === card.id);
+    const storageIndex = bucket.findIndex((item) => +item.id === +card.id);
+    let buyBtnHTML = '';
+    if (storageIndex > -1) {
+      buyBtnHTML = `<div class = "control-wrap"><button class = "btn btn-success minus">-</button><span class="card-count">${bucket[storageIndex].count} шт</span></span><button class = "btn btn-success plus">+</button></div>`;
+    } else {
+      buyBtnHTML = `<button class="btn btn-success buy"
+                           type="button">В корзину
+                                </button>`;
+    }
+
     return `
     <div class="card-wrap" data-id="${card.id}">
       <a class="link-card"
@@ -155,9 +173,7 @@ function createHTML(arr) {
         <span>${data.toLocaleDateString('ru', options)}</span>
       </div>
       <div class="buy-wrap">
-            <button class="btn btn-success buy"
-              type="button">В корзину
-            </button>
+            ${buyBtnHTML}
       </div>
       <div class="buyer"><a href="/user/profile/{{this.User.id}}">${card.User.name}</a></div>
     </div>
@@ -169,8 +185,9 @@ function createHTML(arr) {
 
 function updateBucket() {
   const bucket = JSON.parse(localStorage.getItem('bucket'));
-  if (bucket.length < 0) {
-    spanCount.textContent = 0;
+  if (bucket) {
+    if (bucket.length === 0) {
+      spanCount.textContent = 0;
+    } else spanCount.textContent = bucket.map((card) => +card.count).reduce((acc, cur) => acc + cur);
   }
-  spanCount.textContent = bucket.map((card) => +card.count).reduce((acc, cur) => acc + cur);
 }
