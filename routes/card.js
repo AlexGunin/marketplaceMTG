1d24c009e9909493f684a40013db3b736413f22d
AlexGunin <iwaannafly@mail.ru>
2022-02-04 15:27:48 +0300

    add error handling signin, signup

--- routes/card.js
+++ routes/card.js
@@ -6,14 +6,13 @@ const router = express.Router();
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const card = await Card.findOne({ where: { id }, include: [User, City],  raw: true });
+  const card = await Card.findOne({ where: { id }, include: [User, City], raw: true });
   const user = card['User.name'];
   const city = card['City.title'];
   const options = {
     month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric',
   };
   const date = card.createdAt.toLocaleDateString('ru', options);
-  // console.log('==========================================================>',card);
   res.render('card', {
     card, user, city, date,
   });
@@ -28,17 +27,14 @@ router.post('/', async (req, res) => {
   // const {
   //   city, title, body, image, price, state,
   // } = req.body;
-  // console.log('==========================================================>', req.body);
 
   // const cityOne = await City.findOne({ where: { title: city } });
   // const city_id = cityOne ? cityOne.id : (await City.create({ title: city })).id;
   // const newCard = await Card.create({
   //   user_id, title, body, image, city_id, price, state, available: true, views: 0,
   // });
-  // console.log('==========================================================>', image);
   // const cardId = newCard.id;
   // res.redirect(`/card/${cardId}`);
-  console.log(req.body);
 });
 
 router.get('/bucket', (req, res) => {
--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Op } = require('sequelize');
 const { Card, User, City } = require('../db/models');
+const { accessMiddleware, certainIdAccess } = require('../middleware/accessMiddleware');
 
 const router = express.Router();
 // /main
@@ -9,17 +10,28 @@ router.get('/', async (req, res) => {
   res.render('main', allCards);
 });
 router.post('/', async (req, res) => {
-  const { query = '' } = req.body;
+  const { title = '', city } = req.body;
   const allCards = await Card.findAll({
     include: [User, City],
     where: {
-      title: { [Op.like]: `%${query}%` },
+      [Op.and]: [
+        { title: { [Op.like]: `%${title}%` } },
+
+      ],
     },
   });
 
-  res.json({ allCards });
+  if (city) {
+    return res.json({ allCards: allCards.filter((card) => card.City.title === city) });
+  }
+
+  return res.json({ allCards });
 });
-router.get('/bucket', (req, res) => {
+router.get('/bucket/:userId', accessMiddleware, certainIdAccess, (req, res) => {
   res.render('bucket');
+});
+router.get('/city', async (req, res) => {
+  const allCity = await City.findAll();
+  res.json(allCity);
 });
 module.exports = router;
